d389e0d0a5c15cb3141ce905dc8fd3fe25c8e85b
Michael Ford <mtford@gmail.com>
2015-02-22 13:08:34 +0000

    Provide ability to override theme with a template url

--- src/ng-s3upload/directives/s3-upload.js
+++ src/ng-s3upload/directives/s3-upload.js
@@ -119,8 +119,12 @@ angular.module('ngS3upload.directives', []).
         };
       },
       templateUrl: function(elm, attrs) {
-        var theme = attrs.theme || ngS3Config.theme;
-        return 'theme/' + theme + '.html';
+        var url = attrs.templateUrl;
+        if (!url) {
+          var theme = attrs.theme || ngS3Config.theme;
+          url = 'theme/' + theme + '.html';
+        }
+        return url;
       }
     };
   }]);
